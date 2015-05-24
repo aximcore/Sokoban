749056ceadf98e0f6753897508076064b315f926
AximCore <aximcore@gmail.com>
2015-05-24 21:56:02 +0200

    JUnit fájl és Coordinate osztály feltöltése

---
+++ src/main/java/me/aximcore/Coordinate.java
@@ -0,0 +1,29 @@
+package me.aximcore;
+
+/**
+ * Pozíciók tárolásához használt adatstruktúra.
+ */
+public class Coordinate {
+	/**
+	 * Sor index
+	 */
+	public int x;
+	
+	/**
+	 * Oszlop index
+	 */
+	public int y;
+
+	/**
+	 * Konstruktor két paraméteres példányosításhoz.
+	 * @param x sor index
+	 * @param y oszlop index
+	 */
+	public Coordinate(int x, int y){this.x = x; this.y = y;}
+	
+	/**
+	 * Konstruktor paraméter nélküli példányosításhoz.
+	 */
+	public Coordinate(){this.x = 0; this.y = 0;}
+}
+
---
+++ src/test/java/sokoban/GameTest.java
@@ -0,0 +1,104 @@
+package sokoban;
+
+import static org.junit.Assert.*;
+import me.aximcore.Coordinate;
+import me.aximcore.Game;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GameTest {
+	
+	@Test
+	public void getMapTest() {
+		Game g = new Game();
+		
+		assertNotNull(g.getMap());
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void getGamerPosTest(){
+		Game g = new Game();
+		
+		g.setObject(new Object[][] {null});
+		g.getGamerPos(); // túl indexel
+	}
+	
+	@Test
+	public void getGamerPosTest1(){
+		Game g = new Game();
+		
+		g.setObject(new Object[][] {
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, "jatekos", "t", "t", null, null, null, null, null, null},
+				{null, null, "t", "k", "t", "t", null, null, null, null},
+				{null, null, "t", "t", "t", "t", null, null, null, null},
+				{null, null, null, null, "t", "t", "t", "t", null, null},
+				{null, null, null, null, "t", "t", "k", "t", "c", null},
+				{null, null, null, null, null, "t", "t", "t", "c", null},
+				{null, null, null, null, null, null, null, "t", "t", null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+		});
+
+		assertEquals("Jatekos koordinátája x = 1", 1, g.getGamerPos().x);
+		assertEquals("Jatekos koordinátája y = 1", 1, g.getGamerPos().y);
+	}
+	
+	@Test
+	public void getGamerPosTest2(){
+		Game g = new Game();
+		
+		g.setObject(new Object[][] {
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, "t", "t", "t", null, null, null, null, null, null},
+				{null, null, "t", "k", "t", "t", null, null, null, null},
+				{null, null, "t", "t", "t", "t", null, null, null, null},
+				{null, null, null, null, "t", "t", "t", "t", null, null},
+				{null, null, null, null, "t", "t", "k", "t", "c", null},
+				{null, null, null, null, null, "t", "t", "t", "c", null},
+				{null, null, null, null, null, null, null, "t", "t", null},
+				{null, null, null, null, null, null, null, null, null, null},
+				{null, null, null, null, null, null, null, null, null, null},
+		});
+		
+		assertEquals("Ha nincs jatekos akkor x = 0", 0, g.getGamerPos().x);
+		assertEquals("Ha nincs jatekos akkor y = 0", 0, g.getGamerPos().y);
+	}
+	
+	@Test
+	public void stepTest(){
+		Game g = new Game();
+		
+		g.setClickedPos(1, 2); // t helyre
+		g.setClickedValue("t");
+		g.step();	// játékos átkerül 1 sor 2 oszlopba
+		
+		assertEquals("Jatekos átkerül x = 1-be", 1, g.getGamerPos().x);
+		assertEquals("Jatekos átkerül y = 2-be", 2, g.getGamerPos().y);
+		assertEquals("Egyet lépet", 1, Integer.valueOf(g.getStepCount()).intValue());
+
+	}
+	
+	@Test
+	public void stepTest1(){
+		Game g = new Game();
+		
+		g.setClickedPos(1, 3); // t helyre
+		g.setClickedValue("t");
+		g.step();	// játékos nem lép mert nem egyet lépne
+		
+		assertEquals("Játékos marad x = 1-ben", 1, g.getGamerPos().x);
+		assertEquals("Játékos marad y = 1-ben", 1, g.getGamerPos().y);
+		assertEquals("Nem lepet egyet sem", 0, Integer.valueOf(g.getStepCount()).intValue());
+	}
+	
+	@Test(expected=NullPointerException.class)
+	public void setTableValueTest(){
+		Game g = new Game();
+		
+		g.setTableValue("t"); // step-ben lesz foglalva actPos-nak memória
+	}
+
+}
